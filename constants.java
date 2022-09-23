53826df94141c4d4111ce37a63d694b429c85d3e
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-23 09:37:02 +0530

    Declaring constants

---
+++ constants.java
@@ -0,0 +1,16 @@
+class Data {
+    static final int INTEGER = 20;
+    static final String STRING = "Hello";
+    static final float FLOAT = 1645.23f;
+    static final char CHARACTER = 'C';
+    static final double DOUBLE = 1234.3456;
+}
+public class constants{
+    public static void main(String[] args) {
+        System.out.println("Value of Integer Constant: "+Data.INTEGER);
+        System.out.println("Value of String constant: "+Data.STRING);
+        System.out.println("Value of Float constant: "+Data.FLOAT);
+        System.out.println("Value of Character constant: "+Data.CHARACTER);
+        System.out.println("Value of Double constant: "+Data.DOUBLE);
+    }
+}
