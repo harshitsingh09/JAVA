6761fad5655114b1f06adc121be4694a7bad8ba9
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-25 19:40:04 +0530

    Primitive Data types

---
+++ primDataTypes.java
@@ -0,0 +1,20 @@
+public class primDataTypes{
+    public static void main(String[] args) {
+        byte num1 = 32; // Stores whole numbers from -128 to 127
+        short num2 = 54; // Stores whole numbers from -32,768 to 32,767
+        int num3 = 154; // Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        long num4 = 1432; // Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        float num5 = 1.53f; // Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+        double num6 = 132.565423; // Stores fractional numbers. Sufficient for storing 15 decimal digits
+        boolean num7 = true; // Stores true or false values
+        char letter = 'A'; // Stores a single character/letter or ASCII values
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(num7);
+        System.out.println(letter);
+    }
+}
