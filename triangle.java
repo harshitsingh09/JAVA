e3a4d3a770e97114065baa9647b286bbd8cafdb1
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-28 23:52:00 +0530

    work in progress

---
+++ triangle.java
@@ -0,0 +1,8 @@
+public class triangle {
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 1; i < a+1; i++){
+            System.out.println(i);
+        }
+    }
+}
