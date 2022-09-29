c95296b0e63d5853f1ce1ffef1c4231984eceea4
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-29 11:16:29 +0530

    Creating string using literal and NEW keyword

--- stringE.java
+++ stringE.java
@@ -2,7 +2,7 @@ public class stringE {
     public static void main(String[] args) {
         String s1 = "REVA"; //creating a string using Literal method
         char ch[] = {'u','n','i','v','e','r','s','i','t','y'};
-        String s2 = new String(ch); // Converts character to string
+        String s2 = new String(ch); // Creating string using NEW keyword
         String s3 = new String("BENGALURU"); // NEW keyword creates a new object in String class
         System.out.println(s1);
         System.out.println(s2);
