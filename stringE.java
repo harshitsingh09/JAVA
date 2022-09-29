d631d5bf1a3ae3e4093f63698cb6a5d7fae76519
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-29 11:12:02 +0530

    Creating string using literal method

---
+++ stringE.java
@@ -0,0 +1,11 @@
+public class stringE {
+    public static void main(String[] args) {
+        String s1 = "REVA"; //creating a string using Literal method
+        char ch[] = {'u','n','i','v','e','r','s','i','t','y'};
+        String s2 = new String(ch); // Converts character to string
+        String s3 = new String("BENGALURU"); // NEW keyword creates a new object in String class
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
