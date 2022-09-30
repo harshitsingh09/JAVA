82cb4b8f8ff3a7cf934b874fc13923ca18791f66
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-30 09:55:14 +0530

    unsure how to fix the logic

--- onkush_challenge.java
+++ onkush_challenge.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class onkush_challenge {
+public class onkush_challenge {/*
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
         System.out.println("Enter the start value and end value:");
@@ -20,5 +20,30 @@ public class onkush_challenge {
             }
         }
         x.close();
+    }*/
+
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter the start and End value:");
+        int n = x.nextInt();
+        int m = x.nextInt();
+        if(n > 0 && n < 9){
+            String [] arr = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+            for (int i = 0; i < m; i++){
+                System.out.println(arr[i]);
+            }
+        }
+        else{
+            for (int i = 10; i < m + 1; i++){
+                if(i % 2 == 0){
+                    System.out.println("EVEN");
+                }
+                else{
+                    System.out.println("ODD");
+                }
+            }
+        }
+        x.close();
     }
 }
+// work in progress
