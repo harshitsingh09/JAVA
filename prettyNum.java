e8c5f610d8647f1c52736b4ff7a98aa985b6a128
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-03 00:37:18 +0530

    work in progress

--- prettyNum.java
+++ prettyNum.java
@@ -1,13 +1,13 @@
-import java.util.*;
+// import java.util.*;
 public class prettyNum {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println("Enter the range of first set:");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         System.out.println("Enter the range of first set:");
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
 
         
-        int [] arr = new int[sc];
-    }
+//         int [] arr = new int[sc];
+//     }
 }
