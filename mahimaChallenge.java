205b9cfa6a8fb9ba9ced8f9e9be0c6ddc51ee718
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-06 18:42:53 +0530

    stage 2. Im very confused

--- mahimaChallenge.java
+++ mahimaChallenge.java
@@ -2,13 +2,20 @@ public class mahimaChallenge {
     public static void main(String[] args) {
         System.out.println("Butterfly using asterisks!");
         char[][] arr = new char[8][8];
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
                 System.out.print(" ");
                 if(i == j){
                     arr[i][j] = '*';
                     System.out.print(arr[i][j]);
                 }
+                arr[0][j] = '*';
+                    System.out.print(arr[i][j]);
+                arr[j][0] = '*';
+                    System.out.print(arr[i][j]);
+                // arr[0][i] = '*';
+                // arr[7][i] = '*';
+                // System.out.print(arr[i][j]);
             }
             System.out.println();
         }
