eddc5aa8e4b36859caa930f786c867a151b82617
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-06 11:17:06 +0530

    printing a 2 D array

---
+++ mahimaChallenge.java
@@ -0,0 +1,11 @@
+public class mahimaChallenge {
+    public static void main(String[] args) {
+        System.out.println("Butterfly using asterisks!");
+        int[][] arr = new int[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                ("*");
+            }
+        }
+    }
+}
