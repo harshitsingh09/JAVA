eadb543081a1763fb282fb39af72245184b74160
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-10 22:29:33 +0530

    code by Abhishek

--- mahimaChallenge.java
+++ mahimaChallenge.java
@@ -1,21 +1,16 @@
 public class mahimaChallenge {
     public static void main(String[] args) {
-        System.out.println("Butterfly using asterisks!");
-        char[][] arr = new char[8][8];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                System.out.print(" ");
-                if(i == j){
-                    arr[i][j] = '*';
-                    System.out.print(arr[i][j]);
-                }
-                arr[0][j] = '*';
-                    System.out.print(arr[i][j]);
-                arr[j][0] = '*';    
-                    System.out.print(arr[i][j]);
-                // arr[0][i] = '*';
-                // arr[7][i] = '*';
-                // System.out.print(arr[i][j]);
+        for(int i=0,j=9;i!=10 && j!=10;i++,j--){
+            String[] a= new String[10];
+            a[0]="*";
+            a[9]="*";
+            a[i]="*";
+            a[j]="*";
+            for (int k=0;k<10;k++){
+                if (a[k]==null)
+                    System.out.print(" ");
+                else
+                    System.out.print(a[k]);
             }
             System.out.println();
         }
