150551875553f958b16a99cfdf274dd3530242b1
harshitsingh09 <harshit.arvind@gmail.com>
2022-10-13 23:40:31 +0530

    smtg stmg

---
+++ subashChallenge.java
@@ -0,0 +1,13 @@
+/*
+Create a 2d array using only one for loop and no conditional statement
+*/
+
+import java.util.*;
+import java.util.Scanner;
+
+public class subashChallenge {
+    
+    int m = nextInt();
+    int n = nextInt();
+    int[][] arr = new int[m][n];
+}
